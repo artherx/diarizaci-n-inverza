81e033e487a32ced159bfcf073ee3c8e55386cea
artherx <xxartherxx@gmail.com>
2025-08-05 13:02:08 -0500

    :seedling: se añade el archivo de hooks para exportar stores y hooks existentes :seedling:

---
+++ src/utils/hooks/index.ts
@@ -0,0 +1,9 @@
+// Exportar todos los stores especializados
+export { useSegmentStore } from './UseStoreJson';
+export { useAudioStore } from './useAudioStore';
+export { useUIStore } from './useUIStore';
+
+// Exportar hooks existentes
+export { useAssemblyIA } from './assemblyIA.hook';
+export { adapterIA } from './adapterAI.hook';
+export { diferenciaX, MilisegundostoSecongs } from './logica.hook'; 
